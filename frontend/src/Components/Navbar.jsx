589f1f733a0d8f5c0446bf1df4bed907d73fad17
Nina Adora <nirinakaes@gmail.com>
2026-05-12 13:46:09 +0300

    Design Registration form and LogIn form

--- frontend/src/Components/Navbar.jsx
+++ frontend/src/Components/Navbar.jsx
@@ -3,7 +3,6 @@ import React from 'react';
 function Navbar(){
     return(
         <div>
-            <h1>Navbar</h1>
         </div>
     )
 
--- frontend/src/Pages/Register.jsx
+++ frontend/src/Pages/Register.jsx
@@ -2,9 +2,62 @@ import React from 'react';
 
 function Register(){
     return(
-        <div>
-            <h1>Register</h1>
-        </div>
+        <>
+
+        <section className='mt-8 items-center '>
+                <div className='place-items-center'>
+
+                    <h1 className='font-extrabold text-3xl'>MtaaniConnect</h1>
+                    <h2 className='text-blue-300 '>Discover events near you</h2>
+
+                </div>
+
+
+
+                <form className='flex-col mt-5 flex'>
+
+                    <input 
+                    required
+                    type='name'
+                    className='rounded-xl border-zinc-400 m-  px-4 py-2 border-2 mr-20 ml-20 mt-5'
+                    placeholder='Enter your Full Name'></input>
+
+
+                    <input
+                    required
+                    type='email'
+                    className='border-2 border-zinc-400 px-4 py-2 rounded-xl mt-5 r-20 ml-20 mr-20'
+                    placeholder='Enter your email'></input>
+
+                    <input 
+                    required
+                    type='password'
+                    className='rounded-xl border-zinc-400  px-4 py-2 border-2 mr-20 ml-20 mt-5'
+                    placeholder='Enter your password'></input>
+
+                    <input 
+                    required
+                    type='password'
+                    className='rounded-xl border-zinc-400  px-4 py-2 border-2 mr-20 ml-20 mt-5'
+                    placeholder='Confrim your password'></input>
+
+
+                <button className='text-white hover:bg-sky-800 rounded-2xl mt-6 ml-30 mr-30 py-2 bg-sky-600'>CREATE ACCOUNT </button>
+
+            </form>
+
+
+
+
+
+        </section>
+
+
+
+
+
+        </>
+
     )
 
 }
