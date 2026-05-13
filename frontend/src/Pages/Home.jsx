4bb694933f03736acbf78f510418149c4e9df23f
Nina Adora <nirinakaes@gmail.com>
2026-05-13 11:49:53 +0300

    Improving Home layout

--- frontend/src/Pages/Home.jsx
+++ frontend/src/Pages/Home.jsx
@@ -14,6 +14,21 @@ function Home(){
 
         </div>
 
+
+{/*////////////////////////////////////////////Navigation Panel////////////////////////////////////////////////////////*/}
+      
+        <nav className='bg-sky-800  flex gap-100 flex-row py-6'>
+
+            <h2 className=' text-white hover:underline m-1 font-bold'>Home</h2>
+
+            <h2 className='font-bold hover:underline text-white '>Create Event</h2>
+
+            <h2 className='font-bold hover:underline text-white'>Profile</h2>
+
+
+        </nav>
+
+{/*//////////////////////////////////////////////////Search Bar////////////////////////////////////////////////*/}
         <div>
             <form>
 
@@ -23,11 +38,11 @@ function Home(){
                     <input
                     placeholder='Search'
                     type='search'
-                    className='border-2 px-100 mb-4 shadow-2xl  border-zinc-400 rounded-xl'>
+                    className='border-2 px-100 mb-10 shadow-2xl  border-zinc-400 rounded-xl'>
                     
                     </input>
 
-                    <button className='bg-sky-600 py-1 px-2 rounded-xl hover:bg-sky-800 m-4'>Search</button>
+                    <button className='bg-sky-600 py-1 px-2 rounded-xl text-white hover:bg-sky-800 m-4'>Search</button>
 
                 </div>
 
@@ -35,45 +50,34 @@ function Home(){
 
         </div>
 
+
+        <div>
+
+
+        </div>
+{/*////////////////////////////////////////Category Panel//////////////////////////////////////////////////////*/}
+
         <div className='flex  gap-50 flex-cols'>
 
-            <button className=' text-white bg-sky-600 rounded-xl  hover:bg-sky-800 py-3 px-3'>All</button>
-            <button className=' text-white bg-sky-600 py-2 px-3 hover:bg-sky-800 rounded-xl'>Tech</button>
-            <button className='text-white rounded-xl py-2 px-3 hover:bg-sky-800 bg-sky-600'>Sports</button>
-            <button className=' text-white rounded-xl py-2px-3 hover:bg-sky-800 bg-sky-600'>Music</button>
-            <button className='text-white rounded-xl py-2 px-3 hover:bg-sky-800 bg-sky-600'>Fashion</button>
+            <button className=' text-white bg-sky-600 rounded-xl py-1 px-3 hover:bg-sky-800 '>All</button>
+            <button className=' text-white bg-sky-600 py-1 px-3 hover:bg-sky-800 rounded-xl'>Tech</button>
+            <button className='text-white rounded-xl py-1 px-1 hover:bg-sky-800 bg-sky-600'>Sports</button>
+            <button className=' text-white rounded-xl py-1 px-3 hover:bg-sky-800 bg-sky-600'>Music</button>
+            <button className='text-white rounded-xl py-1 px-3 hover:bg-sky-800 bg-sky-600'>Fashion</button>
 
 
 
         </div>
+
+{/*/////////////////////////////////////////////////Event Cards//////////////////////////////////////////////*/}
 
         <div className='border-bg-zinc border-2 py-46 m-6 overflow-auto rounded-xl shadow-2xl '>
-            <h2>Events</h2>
-
-    
-
+            
+            <h2 className='text-xl font-bold'>Events</h2>
 
         </div>
 
-
-        <nav className='bg-sky-200 flex gap-40 flex-row py-6'>
-
-            <h2 className=' text-white font-bold'>Home</h2>
-
-            <h2 className='font-bold text-white '>Create Event</h2>
-
-            <h2 className='font-bold text-white'>Profile</h2>
-
-
-
-
-        </nav>
-
-
-
-
-
-
+/
 
 
 
