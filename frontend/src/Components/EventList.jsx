c958eab9c47e74689957822bde7a01f9bfd5020b
Nina Adora <nirinakaes@gmail.com>
2026-05-14 13:36:28 +0300

    structure of layout

--- frontend/src/Components/EventList.jsx
+++ frontend/src/Components/EventList.jsx
@@ -1,10 +1,34 @@
 import EventCard from './EventCard';
 function EventList(){
     return(
-        <div>
-            <h1>Event List</h1>
-            <EventCard />
+       <>
+
+         {/*///////////////////My Event List/////////////////*/}
+
+      <div className="border-gray-300 border-2 m-6 rounded-xl p-4">
+
+        <h2 className="font-bold text-3xl">My Event List</h2>
+
+
+        {/*///////////////search input and buttton///////*/}
+        <div className="mt-4 flex flex-col gap-3 items-stretch">
+          <input
+          placeholder="Search your events"
+          className="h-12 flex-1 rounded-2xl bg-zinc-300 border-2 px-4 border-gray-200"></input>
+          <button className="rounded-xl text-white bg-sky-800">Search</button>
+
+          
         </div>
+
+
+      </div>
+
+       
+       
+       
+       
+       
+       </>
     )
 
 }
