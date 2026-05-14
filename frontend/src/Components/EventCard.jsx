a516d2ba4591b834b2ef2df1dd2638711285c127
Nina Adora <nirinakaes@gmail.com>
2026-05-14 13:50:53 +0300

    changes in my events configuration

--- frontend/src/Components/EventCard.jsx
+++ frontend/src/Components/EventCard.jsx
@@ -1,9 +1,7 @@
 function EventCard(){
     return(
 
-       { /*///////////////////////Events Card/////////////////////////////*/}
-
-     <section className="shadow-gray-300 border-2 border-gray-200 rounded-3xl m-6">
+    <section className="shadow-gray-300 border-2 border-gray-200 rounded-3xl m-6">
 
             <div className="m-6">
                 <h2 className="text-sm font-bold text-blue-400">MUSIC</h2>
@@ -12,7 +10,7 @@ function EventCard(){
 
             </div>
 
-{/*////////////venue and time////////////////////////////*/}
+             {/*////////////venue and time////////////////////////////*/}
             <div className="flex m-6 flex-row">
 
                 <div className=" rounded-2xl m-3 py-3 border-gray-300 bg-gray-200">
@@ -33,9 +31,9 @@ function EventCard(){
    {/*////////////////Edit and Delete//////////////////////*/}         
 
 
-                <div className="rounded-2xl flex m-6 flex-row m-3 bg-gray-200 justify-end">
+                <div className="rounded-2xl flex  flex-row m-3 bg-gray-200 justify-end">
 
-                    <button className= "bg-zinc-300 rounded-xl m-1 border-zinc-400">Edit</button>
+                    <button className= "bg-zinc-300 flex-2 rounded-xl m-1 border-zinc-400">Edit</button>
 
                     <button className="bg-blue-400 rounded-xl m-1 py-2 text-white">Delete</button>
 
