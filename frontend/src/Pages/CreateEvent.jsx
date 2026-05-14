c71fda78c907361d69766a50cc2d4c4567c8cf4e
Nina Adora <nirinakaes@gmail.com>
2026-05-14 19:43:58 +0300

    making UI changes

--- frontend/src/Pages/CreateEvent.jsx
+++ frontend/src/Pages/CreateEvent.jsx
@@ -76,8 +76,13 @@ const eventData = {
         <>
         <section>
 
+<<<<<<< Updated upstream
             <div className='bg-sky-800 mt-4'>
                 <h2 className='text-2xl text-white font-bold text-center '>Create Event</h2>
+=======
+            <div className='bg-sky-800'>
+                <h2 className=' text-white text-3xl text-center '>Create Event</h2>
+>>>>>>> Stashed changes
 
             </div>
 
